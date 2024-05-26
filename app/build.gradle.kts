05943b0e8c832c1b062e4f5f5b41e1def6872ec7
FabricioQv <fabricioquevedo.sl003@academico.ifsul.edu.br>
2024-05-26 16:57:10 -0300

    Teste

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,10 +26,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
 }
+
 
 dependencies {
 
+
+    implementation ("com.android.volley:volley:1.2.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -38,4 +42,9 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.google.apis:google-api-services-sheets:v4-rev20240402-2.0.0")
+    implementation ("com.google.api-client:google-api-client:2.5.1")
+    implementation ("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+
 }
--- app/src/main/java/com/example/acreditads2023/Evento_activity.java
+++ app/src/main/java/com/example/acreditads2023/Evento_activity.java
@@ -3,10 +3,15 @@ package com.example.acreditads2023;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
+
 public class Evento_activity extends AppCompatActivity {
     private EditText edtTituloEvento;
     private EditText edtAutorEvento;
@@ -49,6 +54,23 @@ public class Evento_activity extends AppCompatActivity {
 
                 EventoDAO eDAO = new EventoDAO(db);
                 eDAO.salvar(e);
+
+                // Google Sheets URL
+                String urlSheets = "https://script.google.com/macros/s/AKfycbzi4Hf_7h7zmVJ6Kt3XFVH2prtuOsOxeM9DfSCJtjPZnB29hEPgnz8TSgOWdIs8ygA/exec?action=evento&tituloEvento=" +
+                        edtTituloEvento.getText().toString() + "&autorEvento=" + edtAutorEvento.getText().toString() +
+                        "&tipoEvento=" + edtTipoEvento.getText().toString() + "&FechaHoraEvento=" + edtFechaHoraEvento.getText().toString();
+
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, urlSheets, new com.android.volley.Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        Log.i("Response", s);
+                    }
+                }, new com.android.volley.Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(com.android.volley.VolleyError error) {
+                        Log.e("Error", error.toString());
+                    }
+                });
             }
         });
 
